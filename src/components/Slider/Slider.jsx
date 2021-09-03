faf80b5574f957b2000899c1a1ef2d25b19ee1a7
Emmanuel Egomson <fegomson@gmail.com>
2021-09-03 13:42:30 -0500

    Create each slide and their backgrounds

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,8 +7,8 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -29,12 +29,92 @@ const Arrow = styled.div`
   opacity: 0.5;
 `
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+`
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`
+
+const Image = styled.img`
+  height: 80%;
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+`
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  outline: none;
+  cursor: pointer;
+`
+
 const Slider = () => {
   return (
     <Container>
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OF ALL NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OF ALL NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f4">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>POPULAR SALE</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OF ALL NEW ARRIVALS
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
