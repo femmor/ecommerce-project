db4f48d32a7461995ab06762ea4f7a93dfe515c8
Emmanuel Egomson <fegomson@gmail.com>
2021-09-04 09:37:52 -0500

    Create categories and categoriesItem components

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
 
 const Categories = () => {
-  return <div>Categories</div>
+  return <Container>Categories</Container>
 }
 
 export default Categories
---
+++ src/components/Categories/CategoriesItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const CategoriesItem = () => {
+  return <Container>Categories Item</Container>
+}
+
+export default CategoriesItem
