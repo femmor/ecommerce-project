9bb6dc7577798ae9522e43002b82193c5cfba425
Emmanuel Egomson <fegomson@gmail.com>
2021-09-04 11:01:28 -0500

    Finish the product categories design

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { categories } from '../../data'
+import CategoriesItem from './CategoriesItem'
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`
 
 const Categories = () => {
-  return <Container>Categories</Container>
+  return (
+    <Container>
+      {categories.map((category) => (
+        <CategoriesItem key={category.id} category={category}></CategoriesItem>
+      ))}
+    </Container>
+  )
 }
 
 export default Categories
--- src/components/Categories/CategoriesItem.jsx
+++ src/components/Categories/CategoriesItem.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 60vh;
+  position: relative;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Info = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`
+const Button = styled.button`
+  color: black;
+  padding: 10px;
+  font-size: 20px;
+  background-color: white;
+  border: 0;
+  outline: none;
+  cursor: pointer;
+`
 
-const CategoriesItem = () => {
-  return <Container>Categories Item</Container>
+const CategoriesItem = ({ category }) => {
+  console.log(category)
+  return (
+    <Container>
+      <Image src={category.img} />
+      <Info>
+        <Title>{category.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
+  )
 }
 
 export default CategoriesItem
